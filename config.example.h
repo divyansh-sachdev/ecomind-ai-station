a83e358caf41891e5ff57702f533cdd2379e363b
Divyansh Sachdev <89719842+divyansh-sachdev@users.noreply.github.com>
2025-10-28 11:15:00 +0530

    Initial commit: PMS5003 UART particulate reader and MQ sensor calibration

---
+++ config.example.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define PMS_RX_PIN       16
+#define PMS_TX_PIN       17
+#define MQ135_PIN        34
+#define BME_SDA_PIN      21
+#define BME_SCL_PIN      22
+
+#endif
